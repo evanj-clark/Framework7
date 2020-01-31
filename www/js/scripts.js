77683d5f28d9939f176913c0729281f3246a5c2a
Clark <e.clark@uleth.ca>
2020-01-30 17:50:18 -0700

    updated stuff

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,7 +1,22 @@
 var app = new Framework7({
-    
-    root: '#app'
-    
+
+    root: '#app',
+    routes: [
+        {
+            path: '/',
+            url: 'index.html'
+        },
+        {
+            path: '/page1/',
+            url: 'pages/page1.html'
+
+        }
+
+
+
+
+]
+
 })
 
 var mainView = app.views.create(".view-main");
